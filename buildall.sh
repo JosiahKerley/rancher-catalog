5bbaa68bcdf2e3cb0be95b7f7c64b1c1808d6003
Josiah Kerley <josiahkerley@gmail.com>
2016-05-11 08:10:18 -0600

    added build script

---
+++ buildall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BASE=josiahkerley
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+for BUILD in `find ./ | grep 'Dockerfile$'`
+do
+  cd `dirname ${BUILD}`
+  TAG=${BASE}/`pwd | rev| cut -d'/' -f1 | rev`
+  docker build -t ${TAG} .
+  cd "${DIR}"
+done
+docker images | grep "${BASE}"
