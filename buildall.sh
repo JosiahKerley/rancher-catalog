dee05bc35e3084b7b6a7c7326a4071b3ceb67d54
Josiah Kerley <josiahkerley@gmail.com>
2016-05-11 11:23:49 -0600

    testing build

--- buildall.sh
+++ buildall.sh
@@ -5,10 +5,10 @@ cd "${DIR}"
 for BUILD in `find ./ | grep 'Dockerfile$'`
 do
   cd `dirname ${BUILD}`
-  TAG=${BASE}/`pwd | rev| cut -d'/' -f1 | rev`
+  TAG=`pwd | rev| cut -d'/' -f1 | rev`
   docker build -t ${TAG} .
-  docker tag ${TAG} ${DOCKER_REPO}${TAG}
-  docker push ${DOCKER_REPO}${TAG}
+  docker tag ${TAG} ${BASE}/${TAG}
+  docker push ${BASE}/${TAG}
   cd "${DIR}"
 done
 docker images | grep "${BASE}"
